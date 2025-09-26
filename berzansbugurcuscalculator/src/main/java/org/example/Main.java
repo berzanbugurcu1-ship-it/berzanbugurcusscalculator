c74f693c49e774692bc244d530b0e8dc082acbfa
berza <berzanbugurcu1@gmail.com>
2025-09-26 22:58:11 +0200

    init commit

---
+++ berzansbugurcuscalculator/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("Welcome to Berzans Calculator");
+            System.out.println("1) Addition (+)");
+            System.out.println("2) Subtraction (-)");
+            System.out.println("3) Multiplication (*)");
+            System.out.println("4) Division (/)");
+            System.out.println("5) Avsluta");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {}
+                case 1:
+                break
+
+
+
+        }
+    }
+}
